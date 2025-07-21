48f96dc258fdaf9422f217b0ad124aa9b31abd28
benjoe93 <blazerbence@gmail.com>
2025-07-21 22:12:28 +0100

    Triangle with EBO
    Triangle rendering variat using EBO (element buffer object)

--- minimal-renderer/src/main.cpp
+++ minimal-renderer/src/main.cpp
@@ -95,22 +95,31 @@ int main(void)
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left
-         0.5f, -0.5f, 0.0f, // right
-         0.0f,  0.5f, 0.0f  // top
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3,    // first triangle
+        1, 2, 3,    // second triangle
     };
 
     // vertex array object
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-
-    // vertex buffer object
-    glGenBuffers(1, &VBO);
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO); // vertex array object
+    glGenBuffers(1, &VBO);      // vertex buffer object
+    glGenBuffers(1, &EBO);      // element buffer object
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
+    // bind and provide data to vbo
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // bind and provide data to ebo
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // linking vertex attribs
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -125,7 +134,7 @@ int main(void)
 
 
     // uncomment this call to draw in wireframe polygons.
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     /* Render loop */
     while (!glfwWindowShouldClose(window))
     {
@@ -139,7 +148,8 @@ int main(void)
         // draw first triangle
         glUseProgram(program);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
@@ -149,6 +159,7 @@ int main(void)
     // optional: de-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(program);
 
     // glfw: terminate, clearing all previously allocated GLFW resources
@@ -157,12 +168,13 @@ int main(void)
 }
 
 
-/* Resize the viewport every time the window size is updated */
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
