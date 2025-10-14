f806859b02a853e885373201913bea2d89f5447e
benjoe93 <blazerbence@gmail.com>
2025-10-14 09:58:42 +0100

    OpenGL custom enum refactor

--- minimal-renderer/src/Renderer.cpp
+++ minimal-renderer/src/Renderer.cpp
@@ -14,34 +14,34 @@ Renderer::Renderer()
     SetBlendFunction(BlendFunc::SRC_ALPHA, BlendFunc::ONE_MINUS_SRC_ALPHA);
     SetBlendEquation(BlendEquation::ADD); // this is the default 
 
-	SetDepthTest(m_depth_buffer);
+    SetDepthTest(m_depth_buffer);
     SetStencilTest(m_stencil_buffer);
 
-	SetWireframeRender(m_wireframe);
+    SetWireframeRender(m_wireframe);
 
-	SetFaceCulling(m_face_culling);
+    SetFaceCulling(m_face_culling);
     SetFaceCullingMode(FaceCullMode::BACK);
 
-	// Log OpenGL stats
-	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
-	std::cout << "Maximum number of vertex attributes supported: " << GetMaxVertexAttribs() << std::endl;
+    // Log OpenGL stats
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Maximum number of vertex attributes supported: " << GetMaxVertexAttribs() << std::endl;
 
-	state = std::make_unique<AppState>();
+    state = std::make_unique<AppState>();
 }
 
 Renderer::~Renderer() {}
 
 void Renderer::Clear() const
 {
-	GLCall(glClearColor(m_background_color.x, m_background_color.y, m_background_color.z, m_background_color.w));
-	GLCall(glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT));
+    GLCall(glClearColor(m_background_color.x, m_background_color.y, m_background_color.z, m_background_color.w));
+    GLCall(glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT));
 }
 
 void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
 {
-	shader.Bind();
-	va.Bind();
-	GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount() , GL_UNSIGNED_INT, nullptr));
+    shader.Bind();
+    va.Bind();
+    GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount() , GL_UNSIGNED_INT, nullptr));
 }
 
 void Renderer::Draw(Model& obj)
@@ -61,8 +61,8 @@ void Renderer::Draw(Model& obj)
 
 void Renderer::Tick(double current_time)
 {
-	m_delta_time = current_time - m_last_frame;
-	m_last_frame = current_time;
+    m_delta_time = current_time - m_last_frame;
+    m_last_frame = current_time;
 }
 
 void Renderer::SetBlending(bool enabled)
@@ -83,13 +83,13 @@ void Renderer::SetDepthTest(bool enabled)
 {
     m_depth_buffer = enabled;
 
-	if (enabled)
+    if (enabled)
     {
         GLCall(glEnable(GL_DEPTH_TEST));
     }
     else
     {
-		GLCall(glDisable(GL_DEPTH_TEST));
+        GLCall(glDisable(GL_DEPTH_TEST));
     }
 }
 
@@ -97,9 +97,9 @@ void Renderer::SetFaceCulling(bool enabled)
 {
     m_face_culling = enabled;
 
-	if (enabled)
+    if (enabled)
     {
-		GLCall(glEnable(GL_CULL_FACE));
+        GLCall(glEnable(GL_CULL_FACE));
     }
     else
     {
@@ -125,20 +125,20 @@ void Renderer::SetWireframeRender(bool enabled)
 {
     m_wireframe = enabled;
 
-	if (enabled)
-	{
-		GLCall(glPolygonMode(GL_FRONT_AND_BACK, GL_LINE));
-	}
+    if (enabled)
+    {
+        GLCall(glPolygonMode(GL_FRONT_AND_BACK, GL_LINE));
+    }
 }
 
-void Renderer::SetDepthFunction(GLenum function)
+void Renderer::SetDepthFunction(TestingFunc function)
 {
-   GLCall(glDepthFunc(function));
+   GLCall(glDepthFunc(GetTestingFunction(function)));
 }
 
-void Renderer::SetStencilFunction(GLenum function, GLint reference_value, GLuint bit_mask)
+void Renderer::SetStencilFunction(TestingFunc function, GLint reference_value, GLuint bit_mask)
 {
-    GLCall(glStencilFunc(function, reference_value, bit_mask));
+    GLCall(glStencilFunc(GetTestingFunction(function), reference_value, bit_mask));
 }
 
 void Renderer::SetStencilMask(GLuint bit_mask)
@@ -146,9 +146,9 @@ void Renderer::SetStencilMask(GLuint bit_mask)
     GLCall(glStencilMask(bit_mask));
 }
 
-void Renderer::SetStencilOperation(GLenum stencil_fail, GLenum depth_fail, GLenum pass)
+void Renderer::SetStencilOperation(StencilOp stencil_fail, StencilOp depth_fail, StencilOp pass)
 {
-    GLCall(glStencilOp(stencil_fail, depth_fail, pass));
+    GLCall(glStencilOp(GetStencilOperation(stencil_fail), GetStencilOperation(depth_fail), GetStencilOperation(pass)));
 }
 
 void Renderer::SetBlendFunction(BlendFunc src_factor, BlendFunc dst_factor)
@@ -180,55 +180,9 @@ void Renderer::SetFrontFace(FrontFace mode)
 
 int Renderer::GetMaxVertexAttribs() const
 {
-	int nrAttr;
-	GLCall(glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttr));
-	return nrAttr;
-}
-
-GLenum Renderer::GetBlendFunction(BlendFunc id) const
-{
-    switch (id)
-    {
-    case ZERO:                      return GL_ZERO;
-    case ONE:                       return GL_ONE;
-    case SRC_COLOR:                 return GL_SRC_COLOR;
-    case ONE_MINUS_SRC_COLOR:       return GL_ONE_MINUS_SRC_COLOR;
-    case DST_COLOR:                 return GL_DST_COLOR;
-    case ONE_MINUS_DST_COLOR:       return GL_ONE_MINUS_DST_COLOR;
-    case SRC_ALPHA:                 return GL_SRC_ALPHA;
-    case ONE_MINUS_SRC_ALPHA:       return GL_ONE_MINUS_SRC_ALPHA;
-    case DST_ALPHA:                 return GL_DST_ALPHA;
-    case ONE_MINUS_DST_ALPHA:       return GL_ONE_MINUS_DST_ALPHA;
-    case CONSTANT_COLOR:            return GL_CONSTANT_COLOR;
-    case ONE_MINUS_CONSTANT_COLOR:  return GL_ONE_MINUS_CONSTANT_COLOR;
-    case CONSTANT_ALPHA:            return GL_CONSTANT_ALPHA;
-    case ONE_MINUS_CONSTANT_ALPHA:  return GL_ONE_MINUS_CONSTANT_ALPHA;
-    default:                        return GL_ZERO;;
-    }
-}
-
-GLenum Renderer::GetBlendEquation(BlendEquation id) const
-{
-    switch (id)
-    {
-    case ADD:               return GL_FUNC_ADD;
-    case SUBTRACT:          return GL_FUNC_SUBTRACT;
-    case REVERSE_SUBTRACT:  return GL_FUNC_REVERSE_SUBTRACT;
-    case MIN:               return GL_MIN;
-    case MAX:               return GL_MAX;
-    default:                return GL_FUNC_ADD;
-    }
-}
-
-GLenum Renderer::GetCullFaceMode(FaceCullMode id) const
-{
-    switch (id)
-    {
-    case BACK:              return GL_BACK;
-    case FRONT:             return GL_FRONT;
-    case FRONT_AND_BACK:    return GL_FRONT_AND_BACK;
-    default:                return GL_BACK;
-    }
+    int nrAttr;
+    GLCall(glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttr));
+    return nrAttr;
 }
 
 Camera& Renderer::GetActiveCamera() const
@@ -236,3 +190,83 @@ Camera& Renderer::GetActiveCamera() const
     std::shared_ptr<Camera> camera = state->cameras[state->active_camera];
     return *camera;
 }
+
+#pragma region GL_Enums
+GLenum Renderer::GetTestingFunction(TestingFunc id) const
+{
+    switch (id)
+    {
+    case TestingFunc::ALWAYS:   return GL_ALWAYS;
+    case TestingFunc::NEVER:    return GL_NEVER;
+    case TestingFunc::LESS:     return GL_LESS;
+    case TestingFunc::EQUAL:    return GL_EQUAL;
+    case TestingFunc::LEQUAL:   return GL_LEQUAL;
+    case TestingFunc::GREATER:  return GL_GREATER;
+    case TestingFunc::NOTEQUAL: return GL_NOTEQUAL;
+    case TestingFunc::GEQUAL:   return GL_GEQUAL;
+    default:                    return GL_ALWAYS;
+    }
+}
+
+GLenum Renderer::GetStencilOperation(StencilOp id) const
+{
+    switch (id)
+    {
+    case StencilOp::KEEP:       return GL_KEEP;
+    case StencilOp::ZERO:       return GL_ZERO;
+    case StencilOp::REPLACE:    return GL_REPLACE;
+    case StencilOp::INCR:       return GL_INCR;
+    case StencilOp::INCR_WRAP:  return GL_INCR_WRAP;
+    case StencilOp::DECR:       return GL_DECR;
+    case StencilOp::DECR_WRAP:  return GL_DECR_WRAP;
+    case StencilOp::INVE:       return GL_INVERT;
+    default:                    return GL_KEEP;
+    }
+}
+
+GLenum Renderer::GetBlendFunction(BlendFunc id) const
+{
+    switch (id)
+    {
+    case BlendFunc::ZERO:                      return GL_ZERO;
+    case BlendFunc::ONE:                       return GL_ONE;
+    case BlendFunc::SRC_COLOR:                 return GL_SRC_COLOR;
+    case BlendFunc::ONE_MINUS_SRC_COLOR:       return GL_ONE_MINUS_SRC_COLOR;
+    case BlendFunc::DST_COLOR:                 return GL_DST_COLOR;
+    case BlendFunc::ONE_MINUS_DST_COLOR:       return GL_ONE_MINUS_DST_COLOR;
+    case BlendFunc::SRC_ALPHA:                 return GL_SRC_ALPHA;
+    case BlendFunc::ONE_MINUS_SRC_ALPHA:       return GL_ONE_MINUS_SRC_ALPHA;
+    case BlendFunc::DST_ALPHA:                 return GL_DST_ALPHA;
+    case BlendFunc::ONE_MINUS_DST_ALPHA:       return GL_ONE_MINUS_DST_ALPHA;
+    case BlendFunc::CONSTANT_COLOR:            return GL_CONSTANT_COLOR;
+    case BlendFunc::ONE_MINUS_CONSTANT_COLOR:  return GL_ONE_MINUS_CONSTANT_COLOR;
+    case BlendFunc::CONSTANT_ALPHA:            return GL_CONSTANT_ALPHA;
+    case BlendFunc::ONE_MINUS_CONSTANT_ALPHA:  return GL_ONE_MINUS_CONSTANT_ALPHA;
+    default:                                   return GL_ZERO;;
+    }
+}
+
+GLenum Renderer::GetBlendEquation(BlendEquation id) const
+{
+    switch (id)
+    {
+    case ADD:                return GL_FUNC_ADD;
+    case SUBTRACT:           return GL_FUNC_SUBTRACT;
+    case REVERSE_SUBTRACT:   return GL_FUNC_REVERSE_SUBTRACT;
+    case MIN:                return GL_MIN;
+    case MAX:                return GL_MAX;
+    default:                                return GL_FUNC_ADD;
+    }
+}
+
+GLenum Renderer::GetCullFaceMode(FaceCullMode id) const
+{
+    switch (id)
+    {
+    case BACK:                return GL_BACK;
+    case FRONT:               return GL_FRONT;
+    case FRONT_AND_BACK:      return GL_FRONT_AND_BACK;
+    default:                                return GL_BACK;
+    }
+}
+#pragma endregion
--- minimal-renderer/src/Renderer.h
+++ minimal-renderer/src/Renderer.h
@@ -36,30 +36,52 @@ static bool GLLogCall(const char* function, const char* file, int line)
     return true;
 }
 
-enum BlendFunc
+enum class TestingFunc
 {
-    ZERO = 0,
-    ONE,
-    SRC_COLOR,
-    ONE_MINUS_SRC_COLOR,
-    DST_COLOR,
-    ONE_MINUS_DST_COLOR,
-    SRC_ALPHA,
-    ONE_MINUS_SRC_ALPHA,
-    DST_ALPHA,
-    ONE_MINUS_DST_ALPHA,
-    CONSTANT_COLOR,
-    ONE_MINUS_CONSTANT_COLOR,
-    CONSTANT_ALPHA,
-    ONE_MINUS_CONSTANT_ALPHA
+    ALWAYS = 0, // The depth test always passes.
+    NEVER,      // The depth test never passes.
+    LESS,       // Passes if the fragment's depth value is less than the stored depth value.
+    EQUAL,      // Passes if the fragment's depth value is equal to the stored depth value.
+    LEQUAL,     // Passes if the fragment's depth value is less than or equal to the stored depth value.
+    GREATER,    // Passes if the fragment's depth value is greater than the stored depth value.
+    NOTEQUAL,   // Passes if the fragment's depth value is not equal to the stored depth value.
+    GEQUAL      // Passes if the fragment's depth value is greater than or equal to the stored depth value.
+};
+enum class StencilOp
+{
+    KEEP = 0,   // The currently stored stencil value is kept.
+    ZERO,       // The stencil value is set to 0.
+    REPLACE,    // The stencil value is replaced with the reference value set with glStencilFunc.
+    INCR,       // The stencil value is increased by 1 if it is lower than the maximum value.
+    INCR_WRAP,   // Same as GL_INCR, but wraps it back to 0 as soon as the maximum value is exceeded.
+    DECR,       // The stencil value is decreased by 1 if it is higher than the minimum value.
+    DECR_WRAP,   // Same as GL_DECR, but wraps it to the maximum value if it ends up lower than 0.
+    INVE        // Bitwise inverts the current stencil buffer value.
+};
+enum class BlendFunc
+{
+    ZERO = 0,                   // Factor is equal to 0
+    ONE,                        // Factor is equal to 1
+    SRC_COLOR,                  // Factor is equal to the SOURCE color vector
+    ONE_MINUS_SRC_COLOR,        // Factor is eqaul to 1 - SOURCE color vector
+    DST_COLOR,                  // Factor is equal to the DESTINATION color vector
+    ONE_MINUS_DST_COLOR,        // Factor is equal to 1 - DESTINATION color vector
+    SRC_ALPHA,                  // Factor is eqaul to the ALPHA component of the SOURCE color vector
+    ONE_MINUS_SRC_ALPHA,        // Factor is equal to 1 - ALPHA component of the SOURCE color vector
+    DST_ALPHA,                  // Factor is eqaul to the ALPHA component of the DESTINATION color vector
+    ONE_MINUS_DST_ALPHA,        // Factor is equal to 1 - ALPHA component of the DESTINATION color vector
+    CONSTANT_COLOR,             // Factor is equal to the CONSTANT color vector
+    ONE_MINUS_CONSTANT_COLOR,   // Factor is equal to 1 - CONSTANT color vector
+    CONSTANT_ALPHA,             // Factor is eqaul to the ALPHA component of the CONSTANT color vector
+    ONE_MINUS_CONSTANT_ALPHA    // Factor is equal to 1 - ALPHA component of the CONSTANT color vector
 };
 enum BlendEquation
 {
-    ADD = 0,
-    SUBTRACT,
-    REVERSE_SUBTRACT,
-    MIN,
-    MAX
+    ADD = 0,            // result = SRC + DST
+    SUBTRACT,           // result = SRC - DST
+    REVERSE_SUBTRACT,   // result = DST - SRC
+    MIN,                // result = min(DST, SRC)
+    MAX                 // result = max(DST, SRC)
 };
 enum FaceCullMode
 {
@@ -69,8 +91,8 @@ enum FaceCullMode
 };
 enum FrontFace
 {
-    CCW = 0,
-    CW
+    CCW = 0,    // Counter-clock wise
+    CW          // Clock wise
 };
 
 struct AppState
@@ -93,7 +115,7 @@ struct AppState
 class Renderer
 {
     private:
-	    glm::vec4 m_background_color = { 0.1f, 0.1f, 0.1f, 1.0f };
+        glm::vec4 m_background_color = { 0.1f, 0.1f, 0.1f, 1.0f };
 
         bool m_blending       = true;
         bool m_depth_buffer   = true;
@@ -108,11 +130,11 @@ class Renderer
         std::unique_ptr<AppState> state;
 
     public:
-	    Renderer();
-	    ~Renderer();
+        Renderer();
+        ~Renderer();
 
-	    void Clear() const;
-	    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+        void Clear() const;
+        void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
         void Draw(Model& obj);
 
         void Tick(double current_time);
@@ -121,38 +143,46 @@ class Renderer
         void SetDepthTest(bool enabled);
         void SetFaceCulling(bool enabled);
         void SetStencilTest(bool enabled);
-	    void SetWireframeRender(bool enabled);
+        void SetWireframeRender(bool enabled);
 
         /* Specifies the comparison function for depth testing. For more info: https://docs.gl/gl3/glDepthFunc */
-        void SetDepthFunction(GLenum function);
+        void SetDepthFunction(TestingFunc function);
+
         /* Specifies the comparison function for stencil testing. For more info: https://docs.gl/gl3/glStencilFunc */
-        void SetStencilFunction(GLenum function, GLint reference_value, GLuint bit_mask);
+        void SetStencilFunction(TestingFunc function, GLint reference_value, GLuint bit_mask);
+
         /* Controls the writing of individual bits in the stencil planes. For more info: https://docs.gl/gl3/glStencilMask */
         void SetStencilMask(GLuint bit_mask);
+
         /* Sets front and back stencil test actions. For more info: https://docs.gl/gl3/glStencilOp
         - stencil_fail: operate when the stencil test fails
         - depth_fail:   operate when stencil passes but depth test fails
         - pass:         operate when both stencil and depth tests pass
         */
-        void SetStencilOperation(GLenum stencil_fail, GLenum depth_fail, GLenum pass);
+        void SetStencilOperation(StencilOp stencil_fail, StencilOp depth_fail, StencilOp pass);
 
         /*Sets the source and destination factors*/
         void SetBlendFunction(BlendFunc src_factor, BlendFunc dst_factor);
+
         /*Sets the operation between source and destination component of the blending equation*/
         void SetBlendEquation(BlendEquation eq);
 
         void SetFaceCullingMode(FaceCullMode mode);
         void SetFrontFace(FrontFace mode);
 
-	    inline void SetBackgroundColor(glm::vec4 new_color) { m_background_color = new_color; }
+        inline void SetBackgroundColor(glm::vec4 new_color) { m_background_color = new_color; }
     private:
-        GLenum GetBlendFunction(BlendFunc id) const;
-        GLenum GetBlendEquation(BlendEquation id) const;
-        GLenum GetCullFaceMode(FaceCullMode id) const;
+        #pragma region GL_Enums
+            GLenum GetTestingFunction(TestingFunc id) const;
+            GLenum GetStencilOperation(StencilOp id) const;
+            GLenum GetBlendFunction(BlendFunc id) const;
+            GLenum GetBlendEquation(BlendEquation id) const;
+            GLenum GetCullFaceMode(FaceCullMode id) const;
+        #pragma endregion
 
     public:
         Camera& GetActiveCamera() const;
-	    int GetMaxVertexAttribs() const;
+        int GetMaxVertexAttribs() const;
 
         inline double GetDeltaTime() const { return m_delta_time; }
         inline float GetDeltaTimeFloat() const { return static_cast<float>(m_delta_time); }
